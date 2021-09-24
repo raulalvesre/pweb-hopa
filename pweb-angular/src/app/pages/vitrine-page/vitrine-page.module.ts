691c081dcff05237cb024c905f06ed1e39df5745
Gustavo Breve Alessio Nachbar - GNC <gustavo.nachbar@iteris.com.br>
2021-09-24 20:13:03 -0300

    Componente  menu finalizado

--- pweb-angular/src/app/pages/vitrine-page/vitrine-page.module.ts
+++ pweb-angular/src/app/pages/vitrine-page/vitrine-page.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { VitrinePageComponent } from './vitrine-page.component';
 import { HeaderModule } from 'src/app/components/header/header.module';
 import { MenuNavBarComponent } from 'src/app/shared/menu-nav-bar/menu-nav-bar.component';
+import { MenuNavBarModule } from 'src/app/shared/menu-nav-bar/menu-nav-bar.module';
 
 
 
 @NgModule({
-  declarations: [VitrinePageComponent, MenuNavBarComponent],
+  declarations: [VitrinePageComponent],
   imports: [
     CommonModule,
-    HeaderModule
+    HeaderModule,
+    MenuNavBarModule
   ],
-  exports:[VitrinePageComponent,MenuNavBarComponent]
+  exports:[VitrinePageComponent]
 })
 export class VitrinePageModule { }
