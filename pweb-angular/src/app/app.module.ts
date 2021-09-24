e34f58498c41b27ddaf36d3aebdb56807be73568
raulalvesre <raul.alves.re@gmail.com>
2021-09-23 21:40:38 -0300

    feat: Add shared, pages and guards folder
    refactor: Remove components folder

--- pweb-angular/src/app/app.module.ts
+++ pweb-angular/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuNavBarComponent } from './components/shared/menu-nav-bar/menu-nav-bar.component';
-import { GenericCardComponent } from './components/generic/generic-card/generic-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuNavBarComponent,
-    GenericCardComponent
   ],
   imports: [
     BrowserModule,
