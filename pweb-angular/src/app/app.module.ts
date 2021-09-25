70f7439086b84877e4a3f726318148fea3ddd562
raulalvesre <raul.alves.re@gmail.com>
2021-09-24 23:16:08 -0300

    fix: Correct AppModule imports

--- pweb-angular/src/app/app.module.ts
+++ pweb-angular/src/app/app.module.ts
@@ -5,12 +5,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
-import { HeaderModule } from './components/header/header.module';
 
 import { SharedModule } from './shared/shared.module';
 import { MaterialModule } from './shared/material/material.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { VitrinePageModule } from './pages/vitrine-page/vitrine-page.module';
 
 @NgModule({
   declarations: [
@@ -23,8 +20,6 @@ import { VitrinePageModule } from './pages/vitrine-page/vitrine-page.module';
     MaterialModule,
     FlexLayoutModule,
     AppRoutingModule,
-    HeaderModule,
-    VitrinePageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
