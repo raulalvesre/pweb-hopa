61e1506c47b7d70a411f7aa46f0d64a1eb02c6ba
Gustavo Breve Alessio Nachbar - GNC <gustavo.nachbar@iteris.com.br>
2021-09-25 23:24:44 -0300

    arrumando import no vitrina page

--- pweb-angular/src/app/pages/vitrine-page/vitrine-page.module.ts
+++ pweb-angular/src/app/pages/vitrine-page/vitrine-page.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VitrinePageComponent } from './vitrine-page.component';
 import { GenericCardModule } from 'src/app/components/generic-card/generic-card.module';
-import { SharedModule } from 'src/app/shared/shared.module';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
