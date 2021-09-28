2529d4ad49fc7220ed5b925b4b66646feed32b8d
Gustavo Breve Alessio Nachbar - GNC <gustavo.nachbar@iteris.com.br>
2021-09-28 09:52:18 -0300

    adicionando tabela de produtos

--- pweb-angular/src/app/pages/cart-page/cart-page.component.ts
+++ pweb-angular/src/app/pages/cart-page/cart-page.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Transaction {
+  item: string;
+  cost: number;
+}
+
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   styleUrls: ['./cart-page.component.css']
 })
-export class CartPageComponent implements OnInit {
 
+export class CartPageComponent{
+  displayedColumns: string[] = ['item', 'cost'];
+  transactions: Transaction[] = [
+    {item: 'Beach ball', cost: 4},
+    {item: 'Towel', cost: 5},
+    {item: 'Frisbee', cost: 2},
+    {item: 'Sunscreen', cost: 4},
+    {item: 'Cooler', cost: 25},
+    {item: 'Swim suit', cost: 15},
+  ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+  getTotalCost() {
+    return this.transactions.map(t => t.cost).reduce((acc, value) => acc + value, 0);
   }
 
 }
